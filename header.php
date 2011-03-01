63a2f078b7a3665041c88b242cbb1ce2760942ad
John Heimkes IV <admin@johnheimkes.com>
2011-02-28 19:03:34 -0600

    initial commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" xml:lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>title</title>
+    <meta name="Author" content="Author" />
+    <meta name="Robots" content="index,all" />
+    <meta name="Keywords" content="Key Words" />
+    <meta name="Description" content="Descritpion" />
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" type="text/css" media="screen" />
+    <link rel="shortcut icon" href="<?php bloginfo('template_url') ?>/images/favicon.ico" />
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+  </head>
+  <body>
+    <div id="wrapper">
+      <div id="header">
+        <h1><a href="<?php bloginfo('url') ?>">Holy shit an h1 with a link!</a></h1>
+        <div id="nav">
+          
+        </div>
+      </div>
